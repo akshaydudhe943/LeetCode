7f38c0f66537d5ea679d39838f57f2676c021854
akshaydudhe943 <akshaydudhe943@gmail.com>
2023-12-30 17:55:29 +0530

    Given an array of integers and integer target, return indices of the two numbers such that they add up to target

---
+++ TwoSum.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        //int arr[]=new int[2] ;
+        for(int i=0 ; i<nums.length ; i++)
+        {
+            for(int j=0 ; j<nums.length ; j++)
+            {
+                if(nums[i]+nums[j]==target)
+                {
+                   if(i==j)
+                    {
+                        continue;
+                    }
+                    else{
+                        nums[0]=i;
+                        nums[1]=j;
+                         //return arr;
+                    }
+                }
+            }
+        }
+        return nums;
+    }
+}
